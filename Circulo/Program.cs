4a2176a7421788b357cd33435ace9a2b1bc1a2fc
Wiltoken <133724975+Wiltoken@users.noreply.github.com>
2025-03-17 17:05:59 -0500

    Calculo para hallar el area de un circulo

--- Circulo/Program.cs
+++ Circulo/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Bienvenido Calculemos un Circulo!  ");
+var radio = 0d;
+var resultado = 0d;
+
+Console.WriteLine("Calculemos un Circulo!  ");
 // pi * radio * radio
+
+Console.WriteLine("Ingrse el valor de radio Circulo!  ");
+
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultado = Math.PI * radio * radio;
+
+Console.WriteLine("El area del circulo es: " + resultado + " m2 ");
+
